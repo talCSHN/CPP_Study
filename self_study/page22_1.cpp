08dc2ff5e7551dd9d88c27a752482512b060420e
talCSHN <yujakinasakoon@gmail.com>
2025-03-18 23:28:23 +0900

    self study Commit

---
+++ self_study/page22_1.cpp
@@ -0,0 +1,25 @@
+///*
+//	사용자로부터 총 5개의 정수 입력 받아서 합 출력
+//	<실행 예>
+//	1번째 정수 입력: 1
+//	2번째 정수 입력: 2
+//	3번째 정수 입력: 3
+//	4번째 정수 입력: 4
+//	5번째 정수 입력: 5
+//*/
+//#include <iostream>
+//
+//int main()
+//{
+//
+//	int inputNum[5];
+//	int sum = 0;
+//	for (int i = 0; i < 5; i++) {
+//		std::cout << i + 1 << "번째 정수 입력: ";
+//		std::cin >> inputNum[i];
+//		sum += inputNum[i];
+//	}
+//	std::cout << "합계: " << sum << std::endl;
+//
+//	return 0;
+//}
