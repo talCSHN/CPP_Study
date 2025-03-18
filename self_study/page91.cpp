84aaec282ea6636f3553cfbf075533b6f9c27367
talCSHN <yujakinasakoon@gmail.com>
2025-03-19 00:10:31 +0900

    self study Commit

---
+++ self_study/page91.cpp
@@ -0,0 +1,22 @@
+///*
+//길이 정보를 인자로 받아서, 해당 길이의 문자열 저장이 가능한 배열 생성, 해당 배열의 주소값 반환하는 함수
+//*/
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <string.h>
+//#include <stdlib.h>
+//using namespace std;
+//
+//char* makeStrAddr(int len) {
+//    char* str = (char*)malloc(sizeof(char) * len);
+//    return str;
+//}
+//
+//int main()
+//{
+//    char* str = makeStrAddr(20);
+//    strcpy(str, "9 to 24 everyday.");
+//    cout << str << endl;
+//    free(str);
+//    return 0;
+//}
