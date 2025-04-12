a37d1ef1f2ba3c9fe7aab97cce287fb8ad6de6ff
talCSHN <yujakinasakoon@gmail.com>
2025-04-12 22:05:13 +0900

    c++ 복습

---
+++ cppSelfStudy/namespace.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//
+//namespace BestComImpl {
+//	void SimpleFunc() {
+//		std::cout << "BesetComImpl SimpleFunc" << std::endl;
+//	}
+//}
+//
+//int main() {
+//
+//	BestComImpl::SimpleFunc();
+//
+//	return 0;
+//}
