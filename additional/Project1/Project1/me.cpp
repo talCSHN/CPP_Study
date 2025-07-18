cf3e556da3a6b3d7d8050d0ef4eff03ef7cf08f8
talCSHN <yujakinasakoon@gmail.com>
2025-07-18 23:05:43 +0900

    Update me.cpp

--- additional/Project1/Project1/me.cpp
+++ additional/Project1/Project1/me.cpp
@@ -2,35 +2,46 @@
 #include <thread>
 #include <mutex>
 #include <windows.h>
+#include <queue>
+#include <chrono>
 using namespace std;
+
+// 생성자 - 소비자 패턴 // 모니터 파-탄
+
 int sex = 0;
 
 int main()
 {
+	queue<int> tasks;
+
 	mutex sexMutex;
-	{
-
-
-	}
-
+	// 생성자
 	thread t1([&]() {
-		for (int i = 0; i < 150; i++) 
+		int task = 0;
+		for (int i = 0; i < 10; ++i)
 		{
 			unique_lock<mutex> sexLock(sexMutex);
-			sex++;
+			tasks.push(task++);
+			cout << "스레드임 -> " << task << endl;
+			//lock_guard<mutex> sexLock2(sexMutex); // 재귀 락 호출로 인한 데드락 발생
 			sexLock.unlock();
-
-			// 오버헤드 존나큰작업
 		}
-	});
-	
-	int cnt = 10;
-	while (0 <= cnt)
+		});
+
+	int task2 = 0;
+	for (int i = 0; i < 10; ++i)
 	{
-		lock_guard<mutex> sexLock(sexMutex);
-		--sex;
-		--cnt;
-		cout << sex;
+		unique_lock<mutex> sexLock(sexMutex);
+		tasks.push(task2++);
+		sexLock.unlock();
+		cout << "메인임 -> " << task2 << endl;
+		this_thread::sleep_for(std::chrono::seconds(2));
+	}
+	int size = tasks.size();
+	for (int i = 0; i < size; ++i)
+	{
+		cout << tasks.front() << endl;
+		tasks.pop();
 	}
 	t1.join();
 	return 0;
